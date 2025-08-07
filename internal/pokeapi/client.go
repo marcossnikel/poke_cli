f79d227e7fd658188559c27e306155d25c40abb8
mmmachado_meli <marcos.mmachado@mercadolivre.com>
2025-08-07 14:11:47 -0300

    feat: add cache into client

--- internal/pokeapi/client.go
+++ internal/pokeapi/client.go
@@ -3,15 +3,19 @@ package pokeapi
 import (
 	"net/http"
 	"time"
+
+	"github.com/marcossnikel/pokecli/internal/pokecache"
 )
 
 type Client struct {
 	httpClient http.Client
+	cache      pokecache.Cache
 }
 
-// Initialize a new http client with a specific timeout.
-func NewClient(timeout time.Duration) Client {
+// Initialize a new http client with a specific timeout and cache
+func NewClient(timeout, cacheInterval time.Duration) Client {
 	return Client{
+		cache: *pokecache.NewCache(cacheInterval),
 		httpClient: http.Client{
 			Timeout: timeout,
 		},
